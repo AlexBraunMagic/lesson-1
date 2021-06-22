bf4db8b4d9d555368db843d322c5bed3a3a86f5e
AlexBraunMagic <cardist93@mail.ru>
2021-06-22 20:15:42 +0300

    added start in package

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,8 @@ function scripts(){
     return src([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',        
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',  
+        'node_modules/start/build/index.js',      
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
