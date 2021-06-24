dde9e3752802b675e607d957930dfb382fd07167
AlexBraunMagic <cardist93@mail.ru>
2021-06-24 21:55:51 +0300

    new changes

--- gulpfile.js
+++ gulpfile.js
@@ -68,11 +68,7 @@ function startwatch(){
 }
 
 function scripts(){
-    return src([
-        'node_modules/jquery/dist/jquery.js',
-        'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',  
-        'node_modules/start/build/index.js',      
+    return src([     
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
